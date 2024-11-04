03023022c1d78b517e8c0525cbcf7cacc17fc2da
yenreh <qherney@gmail.com>
2024-11-04 17:01:14 -0500

    Minor

--- etl/transformacion_servicios.py
+++ etl/transformacion_servicios.py
@@ -5,4 +5,14 @@ import helper
 ryf_conn = helper.get_ryf_conn()
 etl_conn = helper.get_etl_conn()
 
+# Extract
+
+df_mensajeria_servicio = pd.read_sql_table('mensajeria_servicio', ryf_conn)
+df_mensajeria_estado_servicio = pd.read_sql_table('mensajeria_estadoservicio', ryf_conn)
+
+
+
+# Transform
+# Save df_mensajeria_servicio join df_mensajeria_estado_servicio on id = id_servicio
+
 
