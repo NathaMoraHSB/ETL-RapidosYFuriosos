baec7961b466b8c08cece8df30cc807e1abcd67f
yenreh <qherney@gmail.com>
2024-11-04 18:22:21 -0500

    Transformacion acumulating servicios

--- etl/trans_acu_servicios.py
+++ etl/trans_acu_servicios.py
@@ -4,18 +4,19 @@ from sqlalchemy import text
 import helper
 
 # Establish connections
+
 ryf_conn = helper.get_ryf_conn()
 etl_conn = helper.get_etl_conn()
 
 # Extract
+
 df_mensajeria_servicio = pd.read_sql_table('mensajeria_servicio', ryf_conn)
 df_mensajeria_estado_servicio = pd.read_sql_table('mensajeria_estadosservicio', ryf_conn)
 df_clientes_usuario = pd.read_sql_table('clientes_usuarioaquitoy', ryf_conn)
 
 
-# Prepare the data for merging
+# Transform
 
-# Select the columns that are needed for the transformation
 df_mensajeria_estado_servicio = df_mensajeria_estado_servicio[[
     'servicio_id', 'observaciones', 'fecha', 'hora'
 ]].rename(
@@ -26,12 +27,12 @@ df_mensajeria_estado_servicio = df_mensajeria_estado_servicio[[
     }
 )
 
-# Select the columns that are needed for the transformation
 df_clientes_usuario = df_clientes_usuario[[
     'id', 'sede_id'
 ]]
 
-# Transform
+
+# Merge
 
 # Perform the LEFT JOIN operations
 df_merged = (df_mensajeria_servicio.merge(
@@ -46,17 +47,20 @@ df_merged = (df_mensajeria_servicio.merge(
     how='left'
 ))
 
-# Drop the columns that are no longer needed
+# Clean
+
+# Drop columns
 df_merged = df_merged.drop(columns=[
     'id_y', 'servicio_id', 'hora_visto_por_mensajero', 'visto_por_mensajero',
     'descripcion_multiples_origenes'])
 
-# Clean data in columns mensajero_id, mensajero2_id, mensajero3_id replace null values with 0
+# Fill Null values
 df_merged['mensajero_id'] = df_merged['mensajero_id'].fillna(0)
 df_merged['mensajero2_id'] = df_merged['mensajero2_id'].fillna(0)
 df_merged['mensajero3_id'] = df_merged['mensajero3_id'].fillna(0)
 
-# if descripcion_cancelado is "" replace with "N/A"
+# Fill empty strings
+
 df_merged['descripcion_cancelado'] = df_merged['descripcion_cancelado'].replace("", "N/A")
 df_merged['descripcion_pago'] = df_merged['descripcion_pago'].replace("", "N/A")
 df_merged['novedades'] = df_merged['novedades'].replace("", "N/A")
@@ -68,16 +72,9 @@ df_merged = df_merged.rename(
     }
 )
 
-# Print columns
-print(df_merged.columns)
-
-# print count rows
-print(df_merged.shape)
-
 # Drop the existing table if it exists
 with etl_conn.connect() as connection:
     connection.execute(text("DROP TABLE IF EXISTS trans_acu_servicios;"))
 
-
-# Load (if needed)
+# Load
 df_merged.to_sql('trans_acu_servicios', etl_conn, if_exists='replace', index_label='key_trans_servicio')
