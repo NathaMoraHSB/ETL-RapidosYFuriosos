aa7e8c9362c23aad5f175e396222bc63246c7bc1
yenreh <qherney@gmail.com>
2024-11-04 18:25:45 -0500

    Refactor dim_cliente

--- etl/dim_cliente.py
+++ etl/dim_cliente.py
@@ -1,25 +1,30 @@
 import pandas as pd
 import helper
 
+# Establish connections
+
 ryf_conn = helper.get_ryf_conn()
 etl_conn = helper.get_etl_conn()
 
 
-df_cliente = pd.read_sql('SELECT * FROM public.cliente', ryf_conn)
+# Extract
 
-df_ciudad = pd.read_sql('SELECT ciudad_id, nombre AS ciudad, departamento_id FROM public.ciudad', ryf_conn)
+df_cliente = pd.read_sql_table('cliente', ryf_conn)
+df_ciudad = pd.read_sql_table('ciudad', ryf_conn)
+df_departamento = pd.read_sql_table('departamento', ryf_conn)
+df_tipo_cliente = pd.read_sql_table('tipo_cliente', ryf_conn)
 
-df_departamento = pd.read_sql('SELECT departamento_id, nombre AS departamento FROM public.departamento', ryf_conn)
+# Transform
 
-df_tipo_cliente = pd.read_sql('SELECT tipo_cliente_id, nombre AS tipo_cliente FROM public.tipo_cliente', ryf_conn)
+df_ciudad = df_ciudad[['ciudad_id', 'nombre', 'departamento_id']].rename(columns={'nombre': 'ciudad'})
+df_departamento = df_departamento[['departamento_id', 'nombre']].rename(columns={'nombre': 'departamento'})
+df_tipo_cliente = df_tipo_cliente[['tipo_cliente_id', 'nombre']].rename(columns={'nombre': 'tipo_cliente'})
 
 
+# Perform the LEFT JOIN operations
 
 df_cliente_ciudad = pd.merge(df_cliente, df_ciudad, on='ciudad_id', how='left')
-
-
 df_cliente_ciudad_departamento = pd.merge(df_cliente_ciudad, df_departamento, on='departamento_id', how='left')
-
 df_dim_cliente = pd.merge(df_cliente_ciudad_departamento, df_tipo_cliente, on='tipo_cliente_id', how='left')
 
 
@@ -28,8 +33,7 @@ df_dim_cliente = df_dim_cliente[[
     'nombre_contacto', 'ciudad', 'departamento', 'tipo_cliente', 'activo', 'sector'
 ]]
 
-
-
+# Load
 df_dim_cliente.to_sql(
     'dim_cliente',  # Nombre de la tabla destino en la base de datos.
     con=etl_conn,   # Conexión a la base de datos ETL.
