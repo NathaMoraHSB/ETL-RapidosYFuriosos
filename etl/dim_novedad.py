0a2ef2701abd2e8585e973a5882985472f26f7c9
yenreh <qherney@gmail.com>
2024-11-04 19:30:44 -0500

    Dim Novedad

--- etl/dim_novedad.py
+++ etl/dim_novedad.py
@@ -1,30 +1,39 @@
 import pandas as pd
 import helper
 
+# Constants
+
+TABLE_NAME = 'dim_novedad'
+INDEX_NAME = 'key_dim_novedad'
+
+# Establish connections
 
 ryf_conn = helper.get_ryf_conn()
 etl_conn = helper.get_etl_conn()
 
-query = """
-SELECT
-    mn.id AS novedad_id,
-    mn.descripcion AS novedad_descripcion,
-    mn.fecha_novedad,
-    TO_CHAR(mn.fecha_novedad, 'YYYY-MM-DD') AS novedad_fecha,
-    TO_CHAR(mn.fecha_novedad, 'HH24:MI:SS') AS novedad_hora,
-    mt.nombre AS tipo_novedad_nombre
-FROM
-    public.mensajeria_novedadesservicio mn
-LEFT JOIN
-    public.mensajeria_tiponovedad mt ON mn.tipo_novedad_id = mt.id;
-"""
 
-# Leer los datos desde la base de datos 'RAPIDOS-Y_FURIOSOS' a un DataFrame de Pandas
-df_novedad = pd.read_sql(query, ryf_conn)
+# Extract
 
+df_novedad = pd.read_sql_table('mensajeria_novedadesservicio', ryf_conn)
+df_tipo_novedad = pd.read_sql_table('mensajeria_tiponovedad', ryf_conn)
 
-# Verificar duplicados y datos nulos
-df_novedad.drop_duplicates(subset=['novedad_id'], inplace=True)
-df_novedad.fillna({'novedad_descripcion': 'Sin descripción'}, inplace=True)
+# Transform
 
-df_novedad.to_sql('dim_novedad', etl_conn, if_exists='replace', index=False)
+df_novedad = df_novedad[['id', 'descripcion', 'fecha_novedad', 'tipo_novedad_id']].rename(columns={'id': 'novedad_id', 'descripcion': 'novedad_descripcion', 'fecha_novedad': 'novedad_fecha'})
+df_tipo_novedad = df_tipo_novedad[['id', 'nombre']].rename(columns={'id': 'tipo_novedad_id', 'nombre': 'tipo_novedad_nombre'})
+
+# Merge
+
+# Perform the LEFT JOIN operations
+
+df_dim_novedad = pd.merge(df_novedad, df_tipo_novedad, on='tipo_novedad_id', how='left')
+
+# Clean
+
+# Drop columns that are not needed
+
+df_dim_novedad = df_dim_novedad.drop(columns=['tipo_novedad_id'])
+
+# Load
+
+helper.load_data("etl_conn", df_dim_novedad, TABLE_NAME, INDEX_NAME)
