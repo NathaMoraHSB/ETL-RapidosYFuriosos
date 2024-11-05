9a8783578a328981db664405346b6823e843771f
yenreh <qherney@gmail.com>
2024-11-04 19:22:45 -0500

    Dim Mensajero

--- etl/dim_mensajero.py
+++ etl/dim_mensajero.py
@@ -1,37 +1,45 @@
 import pandas as pd
 import helper
 
+# Constants
+
+TABLE_NAME = 'dim_mensajero'
+INDEX_NAME = 'key_dim_mensajero'
+
+# Establish connections
 
 ryf_conn = helper.get_ryf_conn()
 etl_conn = helper.get_etl_conn()
 
 
-# Query para extraer la información de la dimensión mensajero
-query = """
-SELECT
-    cm.id AS mensajero_id,
-    cm.user_id,
-    cm.activo,
-    cm.fecha_entrada,
-    cm.fecha_salida,
-    cm.telefono,
-    cm.url_foto,
-    c.nombre AS ciudad_nombre,
-    au.username,
-    au.first_name,
-    au.last_name,
-    au.email
-FROM
-    public.clientes_mensajeroaquitoy cm
-LEFT JOIN
-    public.ciudad c ON cm.ciudad_operacion_id = c.ciudad_id
-LEFT JOIN
-    public.auth_user au ON cm.user_id = au.id;
-"""
+# Extract
 
-# Leer los datos desde la base de datos 'RAPIDOS-Y_FURIOSOS' a un DataFrame de Pandas
-df_mensajero = pd.read_sql(query, ryf_conn)
+df_clientes_mensajero = pd.read_sql_table('clientes_mensajeroaquitoy', ryf_conn)
+df_ciudad = pd.read_sql_table('ciudad', ryf_conn)
+df_auth_user = pd.read_sql_table('auth_user', ryf_conn)
 
-# Cargar los datos a la base de datos 'ETL_RYF' como la dimensión 'dim_mensajero'
-df_mensajero.to_sql('dim_mensajero', etl_conn, index=False, if_exists='replace')
+# Transform
 
+df_clientes_mensajero = df_clientes_mensajero[['id', 'user_id', 'activo', 'fecha_entrada', 'fecha_salida', 'telefono', 'url_foto', 'ciudad_operacion_id']].rename(columns={'id': 'mensajero_id'})
+df_ciudad = df_ciudad[['ciudad_id', 'nombre']].rename(columns={'nombre': 'ciudad'})
+df_auth_user = df_auth_user[['id', 'username', 'first_name', 'last_name', 'email']]
+
+# Merge
+
+# Perform the LEFT JOIN operations
+
+df_dim_mensajero = pd.merge(df_clientes_mensajero, df_ciudad, left_on='ciudad_operacion_id', right_on='ciudad_id', how='left')
+df_merged = pd.merge(df_dim_mensajero, df_auth_user, left_on='user_id', right_on='id', how='left')
+print(df_merged.columns)
+
+# Clean
+
+# Drop the columns that are not needed (fecha_entrada y fecha_salida)
+df_merged = df_merged.drop(columns=['ciudad_id', 'ciudad_operacion_id', 'id', 'fecha_entrada', 'fecha_salida'])
+
+# Fill empty strings
+df_merged['ciudad'] = df_merged['ciudad'].fillna("N/A")
+
+# Load
+
+helper.load_data("etl_conn", df_merged, TABLE_NAME, INDEX_NAME)
