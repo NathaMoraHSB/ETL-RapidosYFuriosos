ea391a1344f4b482c7e12a7ef09ead13fd53b33c
yenreh <qherney@gmail.com>
2024-11-06 20:25:17 -0500

    General Update

--- etl/trans_novedades.py
+++ etl/trans_novedades.py
@@ -15,21 +15,41 @@ etl_conn = helper.get_etl_conn()
 # Extract
 
 df_mensajeria_novedad_servicio = pd.read_sql_table('mensajeria_novedadesservicio', ryf_conn)
+df_mensajeria_servicio = pd.read_sql_table('mensajeria_servicio', ryf_conn)
 
 # Transform
 
 df_mensajeria_novedad_servicio = df_mensajeria_novedad_servicio[[
     'id', 'fecha_novedad', 'tipo_novedad_id', 'descripcion', 'servicio_id', 'mensajero_id'
-]]
+]].rename(
+    columns={
+        'id': 'novedad_id'
+    }
+)
+
+df_mensajeria_servicio = df_mensajeria_servicio[[
+    'id', 'cliente_id']].rename(
+    columns={
+        'id': 'mensajeria_servicio_id'
+    }
+)
 
 # Merge
 
 # Perform the LEFT JOIN operations
-df_merged = df_mensajeria_novedad_servicio
+df_merged = df_mensajeria_novedad_servicio.merge(
+    df_mensajeria_servicio,
+    how='left',
+    left_on='servicio_id',
+    right_on='mensajeria_servicio_id'
+)
+
 # Clean
 
 # Drop columns
-# df_merged = df_merged.drop(columns=[])
+df_merged = df_merged.drop(columns=[
+    'servicio_id', 'mensajeria_servicio_id'
+])
 
 # Fill empty strings
 
