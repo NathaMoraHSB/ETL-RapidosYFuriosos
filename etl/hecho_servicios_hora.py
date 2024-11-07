08f3a78f8be54f5c33336089d3da271a3b9bfca9
yenreh <qherney@gmail.com>
2024-11-07 03:04:37 -0500

    Add Dimension Hora

--- etl/hecho_servicios_hora.py
+++ etl/hecho_servicios_hora.py
@@ -14,11 +14,26 @@ etl_conn = helper.get_etl_conn()
 
 # Extract
 
-df_trans_servicios = pd.read_sql_table('trans_servicios', etl_conn)
+# Load transformed data
+df_trans_servicios = pd.read_sql_table('trans_servicios', etl_conn).sort_values(
+    by='key_trans_servicio',
+    ascending=True)
+
+# Load dimensions
+df_dim_hora = pd.read_sql_table('dim_hora', etl_conn)
 
 # Transform
 
-df_merged = df_trans_servicios
+# Merge with dim_hora
+# hora_solicitud
+df_trans_servicios['hora_solicitud'] = df_trans_servicios['hora_solicitud'].apply(lambda x: x.strftime('%H') if pd.notnull(x) else None)
+df_merged = pd.merge(
+    df_trans_servicios,
+    df_dim_hora[['key_dim_hora', 'hora']],
+    left_on='hora_solicitud',
+    right_on='hora').drop(columns=['hora', 'hora_solicitud']).rename(columns={'key_dim_hora': 'key_dim_hora_solicitud'})
+
+df_merged = df_merged.groupby('key_dim_hora_solicitud').size().reset_index(name='numero_servicios')
 
 # Load
 
