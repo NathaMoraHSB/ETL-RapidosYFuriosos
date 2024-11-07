de0bb8d9b2c4696ab7329612cd8eaa754cf88574
yenreh <qherney@gmail.com>
2024-11-07 03:13:03 -0500

    Add hecho novedades

--- etl/hecho_novedades.py
+++ etl/hecho_novedades.py
@@ -11,14 +11,50 @@ INDEX_NAME = 'key_hecho_novedades'
 
 etl_conn = helper.get_etl_conn()
 
-
 # Extract
 
-df_trans_novedades = pd.read_sql_table('trans_novedades', etl_conn)
+# Load transformed data
+df_trans_novedades = pd.read_sql_table('trans_novedades', etl_conn).sort_values(
+    by='key_trans_novedad',
+    ascending=True)
 
+# Load dimensions
+
+df_dim_cliente = pd.read_sql_table('dim_cliente', etl_conn)
+df_dim_mensajero = pd.read_sql_table('dim_mensajero', etl_conn)
+df_dim_fecha = pd.read_sql_table('dim_fecha', etl_conn)
+df_dim_novedad = pd.read_sql_table('dim_novedad', etl_conn)
 # Transform
 
-df_merged = df_trans_novedades
+# Merge with dim_cliente
+df_merged = pd.merge(
+    df_trans_novedades,
+    df_dim_cliente[['key_dim_cliente', 'cliente_id']],
+    left_on='cliente_id',
+    right_on='cliente_id').drop(columns=['cliente_id'])
+
+# Merge with dim_mensajero
+df_merged = pd.merge(
+    df_merged,
+    df_dim_mensajero[['key_dim_mensajero', 'mensajero_id']],
+    left_on='mensajero_id',
+    right_on='mensajero_id').drop(columns=['mensajero_id'])
+
+# Merge with dim_novedad
+df_merged = pd.merge(
+    df_merged,
+    df_dim_novedad[['key_dim_novedad', 'tipo_novedad_id']],
+    left_on='tipo_novedad_id',
+    right_on='tipo_novedad_id').drop(columns=['tipo_novedad_id'])
+
+# Merge with dim_fecha
+# fecha_novedad
+df_merged['fecha_novedad'] = pd.to_datetime(df_merged['fecha_novedad'])
+df_merged = pd.merge(
+    df_merged,
+    df_dim_fecha[['key_dim_fecha', 'fecha_id']],
+    left_on=df_merged['fecha_novedad'].dt.strftime('%Y%m%d').astype(int),
+    right_on='fecha_id').drop(columns=['fecha_id', 'fecha_novedad']).rename(columns={'key_dim_fecha': 'key_dim_fecha_novedad'})
 
 # Load
 
