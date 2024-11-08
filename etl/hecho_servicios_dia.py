c9d7a9ce4d2d77f8ec9b85aebc925a3244af8252
yenreh <qherney@gmail.com>
2024-11-08 14:50:22 -0500

    Fix hechos_servicios_dia and hora

--- etl/hecho_servicios_dia.py
+++ etl/hecho_servicios_dia.py
@@ -22,21 +22,45 @@ def create_hecho_servicios_dia():
 
     # Load dimensions
 
+    # Load dimensions
+
+    df_dim_cliente = pd.read_sql_table('dim_cliente', etl_conn)
+    df_dim_mensajero = pd.read_sql_table('dim_mensajero', etl_conn)
+    df_dim_sede = pd.read_sql_table('dim_sede', etl_conn)
     df_dim_fecha = pd.read_sql_table('dim_fecha', etl_conn)
 
     # Transform
-
-    # Merge with dim_fecha
-
-    df_trans_servicios['fecha_solicitud'] = pd.to_datetime(df_trans_servicios['fecha_solicitud'])
+    # Merge with dim_cliente
     df_merged = pd.merge(
         df_trans_servicios,
+        df_dim_cliente[['key_dim_cliente', 'cliente_id']],
+        left_on='cliente_id',
+        right_on='cliente_id').drop(columns=['cliente_id'])
+
+    # Merge with dim_mensajero
+    df_merged = pd.merge(
+        df_merged,
+        df_dim_mensajero[['key_dim_mensajero', 'mensajero_id']],
+        left_on='mensajero_id',
+        right_on='mensajero_id').drop(columns=['mensajero_id'])
+
+    # Merge with dim_sede
+    df_merged = pd.merge(
+        df_merged,
+        df_dim_sede[['key_dim_sede', 'sede_id']],
+        left_on='sede_id',
+        right_on='sede_id').drop(columns=['sede_id'])
+
+    # Merge with dim_fecha for all dates
+    # fecha_solicitud
+    df_merged['fecha_solicitud'] = pd.to_datetime(df_merged['fecha_solicitud'])
+    df_merged = pd.merge(
+        df_merged,
         df_dim_fecha[['key_dim_fecha', 'fecha_id']],
-        left_on=df_trans_servicios['fecha_solicitud'].dt.strftime('%Y%m%d').astype(int),
+        left_on=df_merged['fecha_solicitud'].dt.strftime('%Y%m%d').astype(int),
         right_on='fecha_id').drop(columns=['fecha_id', 'fecha_solicitud']).rename(columns={'key_dim_fecha': 'key_dim_fecha_solicitud'})
 
-    df_merged = df_merged.groupby('key_dim_fecha_solicitud').size().reset_index(name='numero_servicios')
-
+    df_merged = df_merged.groupby(['key_dim_fecha_solicitud', 'key_dim_mensajero', 'key_dim_cliente', 'key_dim_sede']).size().reset_index(name='numero_servicios')
 
     # Load
     helper.load_data(df_merged, TABLE_NAME, INDEX_NAME)
--- etl/hecho_servicios_hora.py
+++ etl/hecho_servicios_hora.py
@@ -22,20 +22,46 @@ def create_hecho_servicios_hora():
 
     # Load dimensions
     df_dim_hora = pd.read_sql_table('dim_hora', etl_conn)
+    df_dim_cliente = pd.read_sql_table('dim_cliente', etl_conn)
+    df_dim_mensajero = pd.read_sql_table('dim_mensajero', etl_conn)
+    df_dim_sede = pd.read_sql_table('dim_sede', etl_conn)
+
+    # Transform
+    # Merge with dim_cliente
+    df_merged = pd.merge(
+        df_trans_servicios,
+        df_dim_cliente[['key_dim_cliente', 'cliente_id']],
+        left_on='cliente_id',
+        right_on='cliente_id').drop(columns=['cliente_id'])
+
+    # Merge with dim_mensajero
+    df_merged = pd.merge(
+        df_merged,
+        df_dim_mensajero[['key_dim_mensajero', 'mensajero_id']],
+        left_on='mensajero_id',
+        right_on='mensajero_id').drop(columns=['mensajero_id'])
+
+    # Merge with dim_sede
+    df_merged = pd.merge(
+        df_merged,
+        df_dim_sede[['key_dim_sede', 'sede_id']],
+        left_on='sede_id',
+        right_on='sede_id').drop(columns=['sede_id'])
 
     # Transform
 
-    # Merge with dim_hora
+    # Merge with dim_hora for all hours
     # hora_solicitud
-    df_trans_servicios['hora_solicitud'] = df_trans_servicios['hora_solicitud'].apply(lambda x: x.strftime('%H') if pd.notnull(x) else None)
+    df_merged['hora_solicitud'] = df_merged['hora_solicitud'].apply(lambda x: x.strftime('%H') if pd.notnull(x) else None)
     df_merged = pd.merge(
-        df_trans_servicios,
+        df_merged,
         df_dim_hora[['key_dim_hora', 'hora']],
         left_on='hora_solicitud',
         right_on='hora').drop(columns=['hora', 'hora_solicitud']).rename(columns={'key_dim_hora': 'key_dim_hora_solicitud'})
-
-    df_merged = df_merged.groupby('key_dim_hora_solicitud').size().reset_index(name='numero_servicios')
+    df_merged = df_merged.groupby(['key_dim_hora_solicitud', 'key_dim_mensajero', 'key_dim_cliente', 'key_dim_sede']).size().reset_index(name='numero_servicios')
 
     # Load
 
     helper.load_data(df_merged, TABLE_NAME, INDEX_NAME)
+
+create_hecho_servicios_hora()
